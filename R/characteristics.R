6f1bb60be78e23b53047d5648043d8bf34860922
ttriche <tim.triche@gmail.com>
2018-10-26 19:06:21 -0400

    add `titles` and `characteristics` functions

--- R/characteristics.R
+++ R/characteristics.R
@@ -1,24 +1,23 @@
-#' get the titles of a bunch of GSM entries
+#' get the characteristics of a bunch of GSM entries
 #' 
-#' like it says on the tin. this is usually for labeling purposes. 
+#' like it says on the tin. this is usually for filling out pData/colData
 #' 
 #' @param   x         either a grSet from sesamizeGEO, or a bunch of GSMs
 #' @param   column    name of the column holding the GSMs ("subject") 
 #' @param   cachePath where to cache the GEOmetadb sqlite file (tempdir())
 #'
-#' @return            a retitled grSet (if x is a grSet) or a bunch of titles
+#' @return            whatever is in characteristics_ch1 for the GSMs 
 #' 
 #' @import  GEOmetadb
 #' @import  RSQLite
 #' 
 #' @export 
-titleGEO <- function(x, column="subject", cachePath=NULL) { 
+characteristics <- function(x, column="subject", cachePath=NULL) { 
   if (is(x, "GenomicRatioSet")) {
     if (!column %in% names(colData(x))) {
       stop("You need a column named ", column, " in your colData to run this")
-    } else { 
-      GSMs <- x$subject
     }
+    GSMs <- x$subject
   } else { 
     GSMs <- x
   }
@@ -29,13 +28,9 @@ titleGEO <- function(x, column="subject", cachePath=NULL) {
     getSQLiteFile(destdir=cachePath)
   }
   con <- dbConnect(RSQLite::SQLite(), cacheFile)
-  query <- paste0("SELECT title FROM gsm WHERE gsm IN ('",
+  query <- paste0("SELECT gsm, characteristics_ch1 FROM gsm WHERE gsm IN ('",
                   paste(GSMs, collapse="','"), "')")
-  titles <- dbGetQuery(con, query)$title
-  if (is(x, "GenomicRatioSet")) {
-    colnames(x) <- x$title <- titles
-    return(x)
-  } else { 
-    return(titles)
-  }
+  res <- dbGetQuery(con, query)
+  rownames(res) <- res$gsm
+  return(res[GSMs, "characteristics_ch1"])
 }
---
+++ R/titles.R
@@ -0,0 +1,42 @@
+#' get the titles of a bunch of GSM entries
+#' 
+#' like it says on the tin. this is usually for labeling purposes. 
+#' 
+#' @param   x         either a grSet from sesamizeGEO, or a bunch of GSMs
+#' @param   column    name of the column holding the GSMs ("subject") 
+#' @param   cachePath where to cache the GEOmetadb sqlite file (tempdir())
+#'
+#' @return            a retitled grSet (if x is a grSet) or a bunch of titles
+#' 
+#' @import  GEOmetadb
+#' @import  RSQLite
+#' 
+#' @export 
+titles <- function(x, column="subject", cachePath=NULL) { 
+  if (is(x, "GenomicRatioSet")) {
+    if (!column %in% names(colData(x))) {
+      stop("You need a column named ", column, " in your colData to run this")
+    }
+    GSMs <- x$subject
+  } else { 
+    GSMs <- x
+  }
+  if (is.null(cachePath)) cachePath <- tempdir()
+  cacheFile <- paste(cachePath, "GEOmetadb.sqlite", sep="/")
+  if (!file.exists(cacheFile)) {
+    message("Caching GEOmetadb database...") 
+    getSQLiteFile(destdir=cachePath)
+  }
+  con <- dbConnect(RSQLite::SQLite(), cacheFile)
+  query <- paste0("SELECT gsm, characteristics_ch1 FROM gsm WHERE gsm IN ('",
+                  paste(GSMs, collapse="','"), "')")
+  res <- dbGetQuery(con, query)
+  rownames(res) <- res$gsm
+  res <- res[GSMs] # enforce
+  if (is(x, "GenomicRatioSet")) {
+    colnames(x) <- x$title <- res$title
+    return(x)
+  } else { 
+    return(res$title)
+  }
+}
