8a5da9e89682e7ffb3e31601e9fe33d3382920a4
ttriche <tim.triche@gmail.com>
2018-10-28 13:35:25 -0400

    do not mention extractRanges lest the user try it

--- R/print.dmrcate.output.R
+++ R/print.dmrcate.output.R
@@ -12,7 +12,6 @@ print.dmrcate.output <- function(object) {
   cat("Object of class", class(object), "\n\n")
   if ("DMLs" %in% names(object)) cat("  Raw DMLs saved in object$DMLs.\n")
   if ("DMRs" %in% names(object)) cat("  GRanges of DMRs in object$DMRs.\n")
-  cat("  extractRanges(object) will return DMRs as a GRanges object.\n\n")
   cat("  Input data:", nrow(object$input), "features\n")
   cat("  Stouffer p:", paste(format(range(object$results$Stouffer), 
                                     sci=TRUE, digits=2), collapse=" to "), "\n")
