2adb56758e2a5e2ca9608d218def5ec4f8bd8623
Tim Triche, Jr <tim.triche@gmail.com>
2021-03-29 13:48:35 -0400

    fix getDMRs to work with recent DMRcate

--- R/getDMRs.R
+++ R/getDMRs.R
@@ -4,58 +4,47 @@
 #' If you need that flexibility, you are well advised to use DMRcate directly. 
 #' 
 #' @param x               a GenomicRatioSet 
-#' @param design          a design matrix (NULL)
+#' @param design          a design matrix
 #' @param dropXY          drop sex chromosomes? (TRUE, usually a good idea)
-#' @param impute          impute NAs (TRUE, but beware, this WILL hang on HDF5)
-#' @param coef            which column to fit (or "all") (default is 2)
-#' @param fdr             FDR cutoff (0.05)
+#' @param impute          impute NAs (FALSE; this will hang on HDF5 if TRUE)
+#' @param coef            which column to fit (default is 2)
 #' @param betacutoff      DMRs must have at least this maxbetaFC (0.1)
-#' @param DMLs            keep raw differentially methylated loci to plot (TRUE)
-#' @param parallel        run in parallel? (often unwise; uses mclapply) (FALSE)
-#' @param ...             other arguments to pass to dmrcate
+#' @param ...             other arguments to pass to DMRcate::cpg.annotate
 #' 
-#' @return                a dmrcate.output object
+#' @return                tidied output from DMRcate::dmrcate
 #' 
 #' @import DMRcate
 #'
 #' @export
-getDMRs <- function(x, design=NULL, dropXY=TRUE, impute=TRUE, coef=2, fdr=.05, 
-                    betacutoff=.1, DMLs=TRUE, parallel=FALSE, ...) {
+getDMRs <- function(x, design, dropXY=TRUE, impute=FALSE, coef=2, fdr=.05, 
+                    betacutoff=.1, DMLs=FALSE, parallel=FALSE, ...) {
 
   message("Note: this is a simplified, differential-only version of DMRcate.")
   message("DMRcate is capable of much more involved analyses. Read its manual.")
+  stopifnot(!is.null(design))
 
+  message("Dropping non-CpG probes...")
+  x <- subset(x, substr(rownames(x), 1, 2) == "cg")
   if (dropXY) {
     message("Dropping sex chromosomes if present (dropXY was set to TRUE)...")
     x <- keepSeqlevels(x, paste0("chr", 1:22), pruning.mode="coarse")
   }
   
-  # try and avoid hanging with HDF5-backed SummarizedExperiments
-  if (impute) x <- fixNAs(x) # the alternative is to fail...
-  x <- subset(x, !rowData(x)$mask)
-
-  message("Annotating individual CpGs...")
-  annot <- cpgAnnoByChr(x, design=design, coef=coef, fdr=fdr, parallel=parallel)
-  class(annot) <- union(class(annot), "CpGannotated") # for DMRcate
-    
-  message("Demarcating significant regions...")
-  res <- dmrcate(annot, betacutoff=betacutoff, ...) # add parallel processing?
-  res$results <- with(res, subset(results, Stouffer < fdr)) # be strict
-
-  # add DMRs as GRanges (bypass extractRanges)
-  res$DMRs <- as(res$results$coord, "GRanges") 
-  mcols(res$DMRs) <- res$results[, -1]
-
-  # add DMLs?
-  if (DMLs) {
-    sig <- subset(annot$ID, annot$is.sig)
-    DMLoci <- subset(granges(x), rownames(x) %in% sig)
-    m <- match(names(DMLoci), annot$ID)
-    DMLoci$indfdr <- annot$indfdr[m]
-    DMLoci$betafc <- annot$betafc[m]
-    res$DMLs <- DMLoci
+  # avoid hanging with HDF5 SEs?
+  if (impute) {
+    message("Imputing (this may take a VERY LONG TIME with HDF5 backing)...")
+    x <- fixNAs(x) # the alternative is to fail...
+    message("Done imputing.")
   }
-
+  if ("mask" %in% names(rowData(x))) x <- subset(x, !rowData(x)$mask)
+  
+  message("Annotating individual CpGs...")
+  annot <- cpg.annotate("array", x, design=design, coef=2, fdr=fdr, ...)
+ 
+  message("Demarcating significant regions...")
+  res <- extractRanges(dmrcate(annot, C=2, betacutoff=betacutoff), 
+                       genome=unique(genome(x)))
+  message("Found ", length(res), " DMRs.")
   return(res)
 
 }
