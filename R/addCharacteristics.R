6382c61eb45929c05d042caafcdc1c58b0751bb8
Tim Triche, Jr <tim.triche@gmail.com>
2022-01-23 10:35:10 -0500

    add a bit more flexibility in processing/annotation

--- R/addCharacteristics.R
+++ R/addCharacteristics.R
@@ -2,26 +2,29 @@
 #' 
 #' like it says on the tin. this is usually for filling out pData/colData
 #' 
-#' @param   x         either a grSet from sesamizeGEO, or a bunch of GSMs
+#' @param   x         an RGChannelSet or GenomicRatioSet with $subject, or GSMs
 #' @param   column    name of the column holding the GSMs ("subject") 
 #' @param   titles    tack on the titles as well? (TRUE)
 #' @param   cachePath where to cache the GEOmetadb sqlite file (tempdir())
 #'
-#' @return            a grSet, perhaps with additional colData, or a data.frame
+#' @return            an rg/grSet, perhaps with more colData, or a data.frame
 #' 
 #' @import  GEOmetadb
 #' @import  RSQLite
 #' 
 #' @export 
 addCharacteristics <- function(x, column="subject",titles=TRUE,cachePath=NULL) {
-  if (is(x, "GenomicRatioSet")) {
+
+  if (is(x, "GenomicRatioSet") | is(x, "RGChannelSet")) {
     if (!column %in% names(colData(x))) {
       stop("You need a column named ", column, " in your colData to run this")
+    } else { 
+      GSMs <- x$subject
     }
-    GSMs <- x$subject
   } else { 
     GSMs <- x
   }
+
   if (is.null(cachePath)) cachePath <- tempdir()
   cacheFile <- paste(cachePath, "GEOmetadb.sqlite", sep="/")
   if (!file.exists(cacheFile)) {
@@ -33,7 +36,9 @@ addCharacteristics <- function(x, column="subject",titles=TRUE,cachePath=NULL) {
                   paste(GSMs, collapse="','"), "')")
   res <- dbGetQuery(con, query)
   rownames(res) <- res$gsm
+ 
   if (!all(GSMs %in% res$gsm)) stop("Missing records! Aborting.")
+  
   toParse <- res[GSMs, "characteristics_ch1"]
   parsed <- do.call(rbind, lapply(strsplit(toParse,";\t"), elts,sep=": ",elt=2))
   colnames(parsed) <- sapply(strsplit(toParse[1], ";\t")[[1]], elts, sep=":")
@@ -41,10 +46,12 @@ addCharacteristics <- function(x, column="subject",titles=TRUE,cachePath=NULL) {
   parsed <- as.data.frame(parsed)
   if (titles) parsed$title <- addTitles(GSMs)
   parsed$gsm <- GSMs
-  if (is(x, "GenomicRatioSet")) {
+
+  if (is(x, "GenomicRatioSet") | is(x, "RGChannelSet")) {
     for (i in names(parsed)) colData(x)[, i] <- parsed[,i]
     return(x)
   } else {
     return(parsed)
   }
+
 }
--- R/processFromIDATs.R
+++ R/processFromIDATs.R
@@ -3,19 +3,47 @@
 #' Note: this function simply reads all IDATs in the current directory, 
 #'       runs QC, stuffs that into metadata, and sesamizes the result. 
 #'
-#' @param ...   options to pass to sesame::sesamize
+#' @param ...     options to pass to sesame::sesamize
+#' @param addgeo  optional: try to annotate from GEO? (FALSE) 
 #'
-#' @return a GenomicRatioSet 
+#' @return a GenomicRatioSet (or an rgSet in case of failure)
 #' 
 #' @export 
-processFromIDATs <- function(...) {
+processFromIDATs <- function(..., addgeo=FALSE) {
 
   targets <- getSamps() 
   rgSet <- read.metharray.exp(".", targets=targets)
+
+  # QC (document how the SNPs are done...) 
   colnames(rgSet) <- rgSet$subject
-  grSet <- sesamize(rgSet, ...)
-  metadata(grSet)$control_metrics <- t(control_metrics(rgSet)) 
-  metadata(grSet)$control_flagged <- t(flag_control_failures(rgSet)) 
-  return(grSet)
+  metadata(rgSet)$SNPs <- getSnpBeta(rgSet)
+  metadata(rgSet)$control_metrics <- t(control_metrics(rgSet)) 
+  metadata(rgSet)$control_flagged <- t(flag_control_failures(rgSet)) 
+
+  # annotate?
+  if (addgeo) { 
+    message("Attempting to pull metadata...")
+    res <- try(addCharacteristics(rgSet))
+    if (inherits(res, "try-error")) {
+      message("Failed, returning unmodified results.")
+    } else {
+      message("Success!")
+      rgSet <- res
+    }
+  }
+
+  # create a masked GenomicRatioSet
+  grSet <- try(sesamize(rgSet, ...))
+  if (inherits(grSet, "try-error")) {
+    message("Failed to create GenomicRatioSet! Returning raw RGChannelSet.")
+    return(rgSet)
+  } else { 
+    metadata(grSet) <- metadata(rgSet)
+    colData(grSet)$inferred_sex <- getSex(grSet)
+    # sesame's version of this is better but for now just use minfi's 
+    return(grSet)
+  }
+  # eventually, it would be better to create a grSet-like object that wraps 
+  # a FileSet but links to appropriate metadata, e.g. a GenomicFileSet 
 
 }
