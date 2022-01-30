4f8bb60c4ee8cfed695fcd808715fd085a3bfb58
Tim Triche, Jr <tim.triche@gmail.com>
2022-01-30 15:32:10 -0500

    add plotSNPcalls()

---
+++ R/plotSNPcalls.R
@@ -0,0 +1,24 @@
+#' call and plot SNPs from a matrix of SNP betas 
+#' 
+#' plot SNPs living in metadata(x)$SNPs, provided they match sampleNames(x)
+#' 
+#' @param x       a grSet with SNPs in its metadata()
+#' @param rotate  rotate the subjects onto the side? (FALSE)
+#' @param ...     other arguments passed on to Heatmap
+#' 
+#' @import ComplexHeatmap
+#' @import circlize
+#' 
+#' @export
+plotSNPcalls <- function(x, rotate=FALSE, ...) { 
+
+  SNP <- colorRamp2(seq(0, 2), c("#00007F", "yellow", "#7F0000"))
+  SNPs <- as.matrix(metadata(x)$SNPs) # handle DelayedMatrix SNPs
+  if (is.null(SNPs) | !all(colnames(x) %in% colnames(SNPs))) {
+    stop("Your SNPs don't match your samples. Aborting.")
+  }
+  SNPcalls <- SNPcalls(SNPs[, colnames(x)])
+  if (rotate) SNPcalls <- t(SNPcalls)
+  Heatmap(SNPcalls, col=SNP, name="Alleles", ...)
+
+}
