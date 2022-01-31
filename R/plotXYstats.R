29e984640a2f4f93a93c53b99fcbaa8430ad86d3
Tim Triche, Jr <tim.triche@gmail.com>
2022-01-31 07:02:42 -0500

    add column spec to plotXYstats

--- R/plotXYstats.R
+++ R/plotXYstats.R
@@ -1,7 +1,8 @@
 #' simple function for plotting XY stats (split by inferred sex if found)
 #' 
-#' @param grSet a GenomicRatioSet
-#' @param ...   additional parameters to pass to ComplexHeatmap::Heatmap
+#' @param grSet   a GenomicRatioSet
+#' @param column  optional column name with sex information
+#' @param ...     additional parameters to pass to ComplexHeatmap::Heatmap
 #' 
 #' @return a Heatmap
 #' 
@@ -9,14 +10,21 @@
 #' @import circlize
 #' 
 #' @export
-plotXYstats <- function(grSet, ...) {
+plotXYstats <- function(grSet, column=NULL, ...) {
 
   if ("XYstats" %in% names(metadata(grSet))) {
     XY <- metadata(grSet)$XYstats
   } else {
     XY <- XYstats(grSet)
   }
-  if ("inferred_sex" %in% names(colData(grSet))) {
+  if (!is.null(column) & column %in% names(colData(grSet))) {
+    message("Plotting based on `", column, "`...")
+    Heatmap(XY, name="frac", col=jet, column_split=colData(grSet)[,column], ...)
+  } else if ("sex" %in% names(colData(grSet))) {
+    message("Plotting based on `sex`...")
+    Heatmap(XY, name="frac", col=jet, column_split=grSet$sex, ...)
+  } else if ("inferred_sex" %in% names(colData(grSet))) {
+    message("Plotting based on `inferred_sex`...")
     Heatmap(XY, name="frac", col=jet, column_split=grSet$inferred_sex, ...)
   } else {
     Heatmap(XY, name="frac", col=jet, ...)
